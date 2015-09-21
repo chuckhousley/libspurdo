3a0d456b769984f2acae735f3b354258bd016193
gen2 <gen2@kek.club>
2015-09-21 16:07:37 +0100

    Add randomized ebin faces, reformat shit.

--- libspurdo.js
+++ libspurdo.js
@@ -13,87 +13,104 @@
 +along with this program.  If not, see <http://www.gnu.org/licenses/>
 +*/
 
-function toSpurdo(string) {
-    // define replacements
-    replacements = [
-        ["[.]", " :DD"],
-        [",", " XDD"],
-        ["''", ""],
+var ebinFaces = [":D", ":DD", ":DDD", ":-D", "XD", "XXD", "XDD", "XXDD"];
+function getEbinFace () {
+	return ebinFaces[Math.floor(Math.random() * ebinFaces.length)];
+}
 
-        ["wh", "w"],
-        ["th", "d"],
+// define replacements
+var replacements = [
+	["wh", "w"],
+	["th", "d"],
 
-        ["af", "ab"],
-        ["ap", "ab"],
-        ["ca", "ga"],
-        ["ck", "gg"],
-        ["co", "go"],
-        ["ev", "eb"],
-        ["ex", "egz"],
-        ["et", "ed"],
-        ["iv", "ib"],
-        ["it", "id"],
-        ["ke", "ge"],
-        ["nt", "nd"],
-        ["op", "ob"],
-        ["ot", "od"],
-        ["po", "bo"],
-        ["pe", "be"],
-        ["pi", "bi"],
-        ["up", "ub"],
-        ["va", "ba"],
+	["af", "ab"],
+	["ap", "ab"],
+	["ca", "ga"],
+	["ck", "gg"],
+	["co", "go"],
+	["ev", "eb"],
+	["ex", "egz"],
+	["et", "ed"],
+	["iv", "ib"],
+	["it", "id"],
+	["ke", "ge"],
+	["nt", "nd"],
+	["op", "ob"],
+	["ot", "od"],
+	["po", "bo"],
+	["pe", "be"],
+	["pi", "bi"],
+	["up", "ub"],
+	["va", "ba"],
 
-        ["ck", "gg"],
-        ["cr", "gr"],
-        ["kn", "gn"],
-        ["lt", "ld"],
-        ["mm", "m"],
-        ["nt", "dn"],
-        ["pr", "br"],
-        ["ts", "dz"],
-        ["tr", "dr"],
+	["ck", "gg"],
+	["cr", "gr"],
+	["kn", "gn"],
+	["lt", "ld"],
+	["mm", "m"],
+	["nt", "dn"],
+	["pr", "br"],
+	["ts", "dz"],
+	["tr", "dr"],
 
-        ["bs", "bz"],
-        ["ds", "dz"],
-        ["es", "es"],
-        ["fs", "fz"],
-        ["gs", "gz"],
-        [" is", " iz"],
-        ["ls", "lz"],
-        ["ms", "mz"],
-        ["ns", "nz"],
-        ["rs", "rz"],
-        ["ss", "sz"],
-        ["ts", "tz"],
-        ["us", "uz"],
-        ["ws", "wz"],
-        ["ys", "yz"],
+	["bs", "bz"],
+	["ds", "dz"],
+	["es", "es"],
+	["fs", "fz"],
+	["gs", "gz"],
+	[" is", " iz"],
+	["ls", "lz"],
+	["ms", "mz"],
+	["ns", "nz"],
+	["rs", "rz"],
+	["ss", "sz"],
+	["ts", "tz"],
+	["us", "uz"],
+	["ws", "wz"],
+	["ys", "yz"],
 
-        ["alk", "olk"],
-        ["ing", "ign"],
+	["alk", "olk"],
+	["ing", "ign"],
 
-        ["ic", "ig"],
-        ["ng", "nk"],
+	["ic", "ig"],
+	["ng", "nk"],
 
-        ["kek", "geg"],
-        ["epic", "ebin"],
-    ];
+	["kek", "geg"],
+	["epic", "ebin"],
+	["some", "sum"],
+	["meme", "maymay"],
+];
 
-    // apply replacements
-    replacements.forEach(function(filter) {
-        replaceFrom = new RegExp(filter[0], "gi");
-        string = string.replace(replaceFrom, filter[1]);
-    });
+function toSpurdo (string) {
+	// apply replacements
+	replacements.forEach(function(filter) {
+		var replaceFrom = new RegExp(filter[0], "gm"),
+			replaceTo = filter[1];
 
-    // append ":D" if not found
-    if (string.indexOf(":D") < 0) {
-        string = string + " :DD";
-    }
+		string = string.replace(replaceFrom, replaceTo);
+	});
 
-    // return spurdo'd text
-    return string;
+	// Replace "," and "." with ebin faces
+	while (string.match(/\.|,(?=\s|$)/m)) {
+		string = string.replace(/\.|,(?=\s|$)/m, " " + getEbinFace());
+	}
+
+	// append an ebin face if not found
+	var ebinFaceFound = false;
+	ebinFaces.forEach(function (face) {
+		if (string.indexOf(face) != -1) {
+			ebinFaceFound = true;
+		}
+	});
+
+	if (!ebinFaceFound) {
+		string += " " + getEbinFace();
+	}
+
+	// return spurdo'd text
+	return string;
 }
 
 if (typeof module != "undefined") {
-    module.exports = toSpurdo
+	module.exports = toSpurdo
 }
